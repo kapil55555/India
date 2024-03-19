e8683db527ae219d0b56ce6f7abddaf9e2aed97d
kapil55555 <theharshitkumar500@gmail.com>
2024-03-19 23:33:34 +0530

    First commit

--- src/com/example/Maharashtra.java
+++ src/com/example/Maharashtra.java
@@ -5,5 +5,6 @@ public class Maharashtra {
 	public static void main(String[] args) {
 		System.out.println("Maharashtra population is 12 crore");
 	    System.out.println("i love maharashtra");
+	    System.out.println("samruddhi mahamarg from nagpur to mumbai");
 	}
 }
